940bd717cf40fb7f08265b992114b0bc78b5498b
ashwin-nath-m <50395482+ashwin-nath-m@users.noreply.github.com>
2024-12-01 00:33:34 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,22 +7,22 @@ let package = Package(
     name: "TimelineTableViewCell",
     platforms: [.iOS(.v9)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TimelineTableViewCell",
             targets: ["TimelineTableViewCell"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // Add dependencies here if needed
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TimelineTableViewCell",
             dependencies: [],
-            path: "TimelineTableViewCell"),
+            path: "TimelineTableViewCell",
+            resources: [
+                .process("TimelineTableViewCell.xib") // Adjust the filename if it's a .nib
+            ]),
     ],
     swiftLanguageVersions: [.v5]
 )
+
